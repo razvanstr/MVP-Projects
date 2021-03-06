893b3104e0bf19eb6134a4596108fe49582f243f
Razvan <razvan.streuneanu@gmail.com>
2021-03-06 14:21:43 +0200

    Initial project

---
+++ XsiZero/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XsiZero
+{
+    public class User
+    {
+        private string name;
+
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public void string setName(string name)
+        {
+            this.name = name;
+        }
+
+        public User()
+        {
+            this.name = name;
+        }
+
+        public User(string name)
+        {
+            this.name = name;
+        }
+
+        public string salut()
+        {
+            StringBuilder sb = new StringBuilder("Hello, ");
+            sb.Append(this.name);
+            sb.Append('!');
+            return sb.ToString();
+        }
+    }
+}
